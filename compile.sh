88ba8995addf9260223860678310ce8fa5afd35e
Georgi Gerganov <ggerganov@gmail.com>
2018-12-27 17:54:15 +0200

    Move examples to subfolder

--- compile.sh
+++ compile.sh
@@ -2,12 +2,12 @@
 
 cur="ggint"
 echo "Compiling ${cur} ... "
-g++ -std=c++11 -O3 ${cur}.cpp -o ${cur}
+g++ -std=c++11 -O3 -I. examples/${cur}.cpp -o ${cur}
 
 cur="find_prime"
 echo "Compiling ${cur} ... "
-g++ -std=c++11 -O3 ${cur}.cpp -o ${cur}
+g++ -std=c++11 -O3 -I. examples/${cur}.cpp -o ${cur}
 
 cur="find_safe_prime"
 echo "Compiling ${cur} ... "
-g++ -std=c++11 -O3 ${cur}.cpp -o ${cur}
+g++ -std=c++11 -O3 -I. examples/${cur}.cpp -o ${cur}
